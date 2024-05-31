b8dbfdc1a75ca1587491b5d7e62e371d127059ef
Vladimir Buskin <VBuskin@ku.de>
2024-05-31 19:43:19 +0200

    New file

---
+++ NI_Libraries.R
@@ -0,0 +1,29 @@
+
+## General
+library("tidyverse")
+library("readxl")
+library("writexl")
+
+## Statistics and ML
+library("EnvStats")
+library("boot")
+library("caret")
+library("xgboost")
+library("lme4")
+library("rms")
+library("MuMIn")
+library("mgcv")
+library("itsadug")
+library("DALEX")
+library("DALEXtra")
+library("lime")
+library("ranger")
+library("randomForest")
+library("e1071")
+library("class")
+library("MASS")
+
+## Neural networks and Python
+library("tensorflow")
+library("keras3")
+library("reticulate")
